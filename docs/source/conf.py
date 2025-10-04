414399d5c800c762e653fbc5f6261f2a7e854e0f
eldavenport <ellendavenport7@gmail.com>
2025-10-03 18:57:33 -0700

    fixing typo in docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [x
+extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
